de299dcf5e8a780150f66c36a7dcfe01dd1d6092
JujubaOhime <ameliadag96@gmail.com>
2020-10-08 18:08:49 -0300

    feito dark/light mode e navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,15 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("packs/jquery")
+require("packs/custom")
+require("packs/bootstrap.bundle")
 
+
+var jQuery = require('jquery')
+
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
